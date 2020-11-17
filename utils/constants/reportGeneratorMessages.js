9ffd59153a7772750ec3a737307c3f80b06a3410
Eyder Ascuntar Rosales <eaar23@gmail.com>
2020-11-17 13:10:50 -0500

    Cambiando parametro de memoria

--- utils/constants/reportGeneratorMessages.js
+++ utils/constants/reportGeneratorMessages.js
@@ -10,7 +10,9 @@ module.exports = {
   E_REPORT_GENERATOR_MS_02:
     'La plantilla que está cargando no es la adecuada para este recurso. Por favor verifique la información',
   E_REPORT_GENERATOR_MS_03: 'Error al cargar la informacipon de la plantilla',
-  E_REPORT_GENERATOR_MS_04: 'El usuario debe tener asociada una empresa para subir información.',
-  E_REPORT_GENERATOR_MS_05: 'No se ha identificado. Por favor inicie sesión para acceder a este recurso',
+  E_REPORT_GENERATOR_MS_04:
+    'El usuario debe tener asociada una empresa para subir información.',
+  E_REPORT_GENERATOR_MS_05:
+    'No se ha identificado. Por favor inicie sesión para acceder a este recurso',
   M_REPORT_GENERATOR_MS_01: 'Plantilla cargada satisfactoriamente'
 };
