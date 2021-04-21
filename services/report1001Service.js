6ebfa1a17022e43ab029c59089a121b447792b1e
Eyder Ascuntar Rosales <eaar23@gmail.com>
2021-04-21 15:26:03 -0500

    quitando validaciones

--- services/report1001Service.js
+++ services/report1001Service.js
@@ -369,7 +369,6 @@ exports.sendReportCSV = async (req, res) => {
     console.log('>>>>>>>> TIEMPO DE INICIO xxxx');
     console.log(new Date());
     const userInfo = await userService.getUserInfo(req, res);
-    console.log(userInfo);
     objectReportResume.companyId = userInfo.companyId;
     objectReportResume.generatorUserId = userInfo._id;
     const reportInfo = await ReportCreator.find({
@@ -401,7 +400,6 @@ exports.sendReportCSV = async (req, res) => {
     const reportData = await Report1001.find({
       companyId: userInfo.companyId
     }).lean();
-    console.log(`ES ESTO? ${reportData.length}`);
     console.log('>>>>>>>>>>>>  cargado en memoria');
 
     const nameFile = 'REPORTE 1001';
